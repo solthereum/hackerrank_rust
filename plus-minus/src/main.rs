6bc7a280b1bcf89cb0497af55f5d060e674cc7d2
solthereum <shining.superstar.1234@gmail.com>
2023-10-13 00:49:00 -0700

    [solved] plus minus

---
+++ plus-minus/src/main.rs
@@ -0,0 +1,45 @@
+use std::io::{self, BufRead};
+
+fn plusMinus(arr: &[i32]) {
+    let length = arr.len();
+    let mut zero_count = 0;
+    let mut plus_count = 0;
+    let mut minus_count = 0;
+
+    for digit in arr {
+        if *digit > 0 {
+            plus_count += 1;
+        } else if *digit < 0 {
+            minus_count += 1;
+        } else {
+            zero_count += 1;
+        }
+    }
+
+    // println!("zero_count: {}, plus_count: {}, minus_count: {}", zero_count, plus_count, minus_count);
+
+    println!("{:.6}", plus_count as f32 / length as f32);
+    println!("{:.6}", minus_count as f32 / length as f32);
+    println!("{:.6}", zero_count as f32 / length as f32);
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next()
+                    .unwrap()
+                    .unwrap()
+                    .trim()
+                    .parse::<i32>()
+                    .unwrap();
+    let arr: Vec<i32> = stdin_iterator.next()
+                            .unwrap()
+                            .unwrap()
+                            .trim_end()
+                            .split(' ')
+                            .map(|s| s.to_string().parse::<i32>().unwrap())
+                            .collect();
+
+    plusMinus(&arr);
+}
